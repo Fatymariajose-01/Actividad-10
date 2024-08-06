a20b9ab032da8f3a98b334110df8eabcce429b68
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-08-06 17:09:21 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 10/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Menú");
+Console.WriteLine("1. Agregar nuevos productos");
+Console.WriteLine("2. Listar todos los producatos registrados");
+Console.WriteLine("3. Buscar producto por nombre");
+Console.WriteLine("4. Salir del programa");
+Console.WriteLine("Elija una opción");
+int opcion = Convert.ToInt32(Console.ReadLine());
+
+switch (opcion)
+{
+    case 1:
+
+        break;
+}
