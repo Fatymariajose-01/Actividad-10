219dae6b7112c3041a47bccb3e42735cd55b944f
Fatymariajose-01 <fmariajose2006@gmail.com>
2024-08-06 17:14:32 -0600

    Agregué la clase Producto que será una o la superclase.

---
+++ Actividad 10/Producto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_10
+{
+    internal class Producto
+    {
+
+        public string Marca { get; set; }
+        public string NumeroDeSerie { get; set; }
+        public string Modelo { get; set; }
+
+        public Producto(string marca, string numeroDeSerie, string modelo)
+        {
+            Marca = marca;
+            NumeroDeSerie = numeroDeSerie;
+            Modelo = modelo;
+        }
+
+        public virtual string MostrarInformacion()
+        {
+            return $"Marca: {Marca}, Número de Serie: {NumeroDeSerie}, Modelo: {Modelo}";
+        }
+    }
+}
